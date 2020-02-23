0d3ab33d1e639a5d5f3908389c6b18dec3127b0a
anhoangphuc <hoangphucnb97@gmail.com>
2020-02-23 22:59:03 +0700

    Second push

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from torchvision.utils import save_image
 
 def get_model():
     checkpoint = "model_check_point/CRAN_V2/CARN_model_checkpoint.pt"
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model_cran_v2 = CARN_V2(color_channels=3, mid_channels=64, conv=nn.Conv2d,
                         scale=2, activation=nn.LeakyReLU(0.1),
                         SEBlock=True, repeat_blocks=3, atrous=(1, 1, 1))
     model_cran_v2 = network_to_half(model_cran_v2)
-    model_cran_v2.load_state_dict(torch.load(checkpoint, 'cpu'))
+    print('device', device)
+    model_cran_v2.load_state_dict(torch.load(checkpoint, device))
     model_cran_v2 = model_cran_v2.float()
 
     return model_cran_v2
@@ -25,6 +27,7 @@ def scale2(img):
     with torch.no_grad():
         out = [model(i) for i in img_patches]
     img_upscale = img_splitter.merge_img_tensor(out)
+    print('Upscale type: ', type(img_upscale))
     return img_upscale
     
 
@@ -44,6 +47,7 @@ if __name__ == '__main__':
     img = img.convert("RGB")
 
     img_t = to_tensor(img).unsqueeze(0) 
+    save_image(img_t, 'original.jpg')
 
     #img = img.resize((img.size[0] // 2, img.size[1] // 2), Image.BICUBIC) 
 
